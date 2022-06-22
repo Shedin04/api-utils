7d9a8b8f39646b5e0b17d3482a49a88cb8baa8a7
Dmytro_Shedin <dmytro.shedin@pearson.com>
2022-06-23 00:26:01 +0300

    refactoring for api part 3

--- src/main/java/com/shedin/apicore/request/BaseRequest.java
+++ src/main/java/com/shedin/apicore/request/BaseRequest.java
@@ -4,40 +4,62 @@ import io.restassured.http.ContentType;
 import io.restassured.response.Response;
 import lombok.experimental.UtilityClass;
 
-import static com.shedin.apicore.constants.StringConstants.RequestParameters.COOKIE;
+import java.util.Map;
+
 import static io.restassured.RestAssured.given;
 
 
 @UtilityClass
 public class BaseRequest {
 
-	public Response getRequest(final String uri, final int statusCode, String token) {
+	public Response getRequest(final String uri, Map<String, String> headers) {
 		return given()
 				.when()
-				.header(COOKIE, token)
+				.headers(headers)
 				.log().method()
 				.log().uri()
 				.log().headers()
 				.get(uri)
 				.then()
-				.statusCode(statusCode)
 				.extract().response();
 	}
 
+	public Response getRequest(final String uri) {
+		return given()
+				.when()
+				.log().method()
+				.log().uri()
+				.log().headers()
+				.get(uri)
+				.then()
+				.extract().response();
+	}
 
-	public Response postRequest(final String uri, final int statusCode, final Object raw, String token) {
+	public Response postRequest(final String uri, final Object raw, Map<String, String> headers) {
 		return given()
 				.contentType(ContentType.JSON)
 				.body(raw)
 				.when()
-				.header(COOKIE, token)
+				.headers(headers)
 				.log().method()
 				.log().body(true)
 				.log().uri()
 				.log().headers()
 				.post(uri)
 				.then()
-				.statusCode(statusCode)
+				.extract().response();
+	}
+
+	public Response postRequest(final String uri, final Object raw) {
+		return given()
+				.contentType(ContentType.JSON)
+				.body(raw)
+				.when()
+				.log().method()
+				.log().body(true)
+				.log().uri()
+				.post(uri)
+				.then()
 				.extract().response();
 	}
 
